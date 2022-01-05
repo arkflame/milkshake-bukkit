3fb1e20be38feaf9fc7f5d7602e2c36662c7f4f8
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2022-01-05 20:05:11 -0300

    updated source

---
+++ src/main/java/dev/_2lstudios/example/MilkshakeBukkit.java
@@ -0,0 +1,10 @@
+package dev._2lstudios.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MilkshakeBukkit extends JavaPlugin {
+    @Override
+    public void onEnable () {
+        // Do nothing
+    }
+}
