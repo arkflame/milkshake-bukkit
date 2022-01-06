69dfc945fa46c00e9c0e7430bd4c23b1fe6d9458
Sammwy <sammwy.dev@gmail.com>
2022-01-06 11:45:59 -0300

    Fixed package name

--- src/main/java/dev/_2lstudios/milkshakebukkit/MilkshakeBukkit.java
+++ src/main/java/dev/_2lstudios/milkshakebukkit/MilkshakeBukkit.java
@@ -1,10 +1,10 @@
-package dev._2lstudios.example;
+package dev._2lstudios.milkshakebukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MilkshakeBukkit extends JavaPlugin {
     @Override
-    public void onEnable () {
+    public void onEnable() {
         // Do nothing
     }
 }
